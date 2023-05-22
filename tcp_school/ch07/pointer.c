93e09cd1b5db7770bc475bd5cd5fb7d34f8946f9
OAO <ashgrayblue0@gmail.com>
2023-05-22 17:28:09 +0900

    test

--- tcp_school/ch07/pointer.c
+++ tcp_school/ch07/pointer.c
@@ -11,6 +11,15 @@ int main(void){
 	{
 		printf("포인터ptr_num01이 가리키고 있는 주소에 저장된 값은 %d입니다.\n", *ptr_num01);
 		printf("포인터ptr_num02이 가리키고 있는 주소에 저장된 값은 %d입니다.\n", *ptr_num02);
-		printf("포인터ptr_num01과 ", *ptr_num01);
+		printf("포인터ptr_num01과 ptr_num02는 현재 다른 주소를 가리키고 있습니다.\n\n");
+		ptr_num02 = ptr_num01; // 포인터끼리의 대입 연산
+	}
+
+	printf("포인터 ptr_num01이 가리키고 있는 주소에 저장된 값은 %d입니다.\n", *ptr_num01);
+	printf("포인터 ptr_num02가 가리키고 있는 주소에 저장된 값은 %d입니다.\n", *ptr_num02);
+
+	if(ptr_num01 == ptr_num02)
+	{
+		printf("포인터 ptr_num01과 ptr_num02는 현재 같은 주소를 가리키고 있습니다.\n");
 	}
 }
